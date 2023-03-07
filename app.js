dabcc5a904cb3f94b0dcd71c9e5cf89e3c50c719
Shola Nur <shola.nur@gmail.com>
2023-03-06 21:46:53 -0800

    quote generator app completed version

---
+++ app.js
@@ -0,0 +1,18 @@
+const button = document.querySelector("#button");
+const quote = document.querySelector("#quote");
+
+function clickButton() {
+  const apiUrl = "https://type.fit/api/quotes";
+  console.log(apiUrl);
+  fetch(apiUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      let dataQuote = data;
+
+      console.log(dataQuote);
+      const randomQuote = Math.floor(Math.random() * dataQuote.length);
+      quote.textContent = dataQuote[randomQuote].text;
+    });
+}
+
+button.addEventListener("click", clickButton);
